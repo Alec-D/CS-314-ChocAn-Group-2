92ad4b481390569ba4824fc2e4a718a19184fd5d
Alec D <asdiefen@gmail.com>
2023-11-29 00:58:46 -0800

    finished employee testing with integration

--- employee_test.py
+++ employee_test.py
@@ -61,16 +61,28 @@ def test_edit_member(new_employee, monkeypatch):
     assert new_employee.file_system.get_member_by_name('Addekin').first_name == 'Changed'
 
 def test_delete_member(new_employee):
-    pass
+    new_employee.delete_member(399310330)
+    assert new_employee.file_system.get_member_by_id(399310330) is None
 
-def test_add_existing_provider(new_employee):
-    pass
+def test_add_existing_provider(new_employee, monkeypatch, capsys):
+    inputs = iter(['Aleixo'])
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    new_employee.add_provider()
+    captured = capsys.readouterr()
+    assert captured.out == "Provider already exists\n"
 
-def test_add_new_provider(new_employee):
-    pass
+def test_add_new_provider(new_employee, monkeypatch):
+    inputs = iter(['Test', 'Test', 'Test', '1234 Test St', 'Test City', 'CA', 12345, 1])
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    new_employee.add_provider()
+    assert new_employee.file_system.get_provider_by_name('Test') is not None
 
-def test_edit_provider(new_employee):
-    pass
+def test_edit_provider(new_employee, monkeypatch):
+    inputs = iter(['Aleixo', 'First Name', 'Changed', 'y', 'y'])
+    monkeypatch.setattr('builtins.input', lambda prompt: next(inputs))
+    new_employee.edit_provider()
+    assert new_employee.file_system.get_provider_by_name('Aleixo').first_name == 'Changed'
 
 def test_delete_provider(new_employee):
-    pass
+    new_employee.delete_provider(939285195)
+    assert new_employee.file_system.get_provider_by_id(939285195) is None
