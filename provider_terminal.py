58a1aac4f1c93f22c32c95b14e2e4e121a0d3cbd
Adrian Perez <adrian6@pdx.edu>
2023-11-29 01:27:31 -0800

    setting up the provider terminal

---
+++ provider_terminal.py
@@ -0,0 +1,29 @@
+from file_system import FileSystem
+from service import Service 
+from member import Member 
+from provider import Provider
+
+class ProviderTerminal(Provider):
+
+    def __init__(self, member, service_number, service_date):
+        self.member = member
+        self.service_number = service_number 
+        self.service_date = service_date
+
+    def run(self):
+
+   # def get_selection(self):
+
+   # def get_date_of_service(self):
+           
+    #def get_service_number(self):
+           
+    #def get_member(self):
+           
+
+    #def get_directory(self):
+         
+
+    #def document_service(self):
+
+    
