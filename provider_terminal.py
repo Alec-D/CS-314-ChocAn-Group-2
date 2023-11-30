f6b0e0c02df0e77d1eb507a54ac47eccaf68ad2b
Adrian Perez <adrian6@pdx.edu>
2023-11-29 23:50:40 -0800

    adding provider terminal

--- provider_terminal.py
+++ provider_terminal.py
@@ -1,40 +1,42 @@
-from file_system import FileSystem
+from file_system import *
 from service import *
 from member import *
-from provider import Provider
-import datetime 
+from provider import *
 
-class ProviderTerminal:
+class ProviderTerminal(Provider):
 
     def __init__(self, member, service_number, service_date, service_time, file_system, provider):
+       
+        self.member = member
+        self.service_number = service_number
+        self.service_date = service_date
+        self.service_time = service_time
+        self.file_system = file_system
+
+      
+    #def run_visit(self):
+     
+
+    def get_service(self):
         self.service_number = Service()
         self.service_number.service_code
         self.service_date = Service()
         self.service_date.current_date
         self.service_time = Service()
         self.service_time.current_time
-        self.file_system = file_system
 
-
-    
-
-
-    #def run_visit(self):
-     
-
-    def get_service(self):
-
-     while True:
-        code = input("Enter the six digit service number: ")
+        while True:
+         code = input("Enter the six digit service number: ")
         
-        print("You entered ",input)
-        print("Checking if it's six digits",input.isdigit())
+         print("You entered ",input)
+         print("Checking if it's six digits",input.isdigit())
         
-        if code == self.service_code:
-             print("This is correct ")
+         if code == self.service_code:
+            print("This is correct ")
+            break
 
-        else:
-           print("Try again")
+         else:
+            print("Try again")
 
     
 
