c00f4b2eb4292205704139d84e2f6d4e1c0e0352
Sarah Dylan <SarahmDylan@gmail.com>
2023-11-30 16:46:34 -0800

    Add doc strings to file and update service service_dir
    Professor mentioned in class that there needs to be a minimum
    of twenty services.

--- file_system.py
+++ file_system.py
@@ -122,6 +122,17 @@ class FileSystem:
                 columns=["id", "first_name", "last_name", "is_manager"])
 
     def _get_provider_report_info(self, prov_id: int | str, start_date: str = None, end_date: str = None) -> pd.DataFrame | None:
+        """
+        Returns a dataframe containing the information needed to generate a provider report. Currently this defaults to
+        the last 7 days, but can be changed by passing in start_date and end_date. This isn't currently supported by
+        the system, but it would be easy to add for future use.
+
+        :param prov_id: the provider id as a string or int
+        :param start_date: start date as a string in the format mm-dd-yyyy
+        :param end_date: end date as a string in the format mm-dd-yyyy
+        :return: dataframe containing the information needed to generate a provider report or None if the provider is
+        not found
+        """
         if isinstance(prov_id, str):
             if len(prov_id) != 9:
                 return None
@@ -168,6 +179,21 @@ class FileSystem:
         return tmp_df
 
     def _get_member_report_info(self, mem_id: int | str, start_date: str = None, end_date: str = None) -> pd.DataFrame | None:
+        """
+        Returns a dataframe containing the information needed to generate a member report. Currently this defaults to
+        the last 7 days, but can be changed by passing in start_date and end_date. This isn't currently supported by
+        the system, but it would be easy to add for future use.
+
+        The logic behind this function is rather confusing as I had a rather hard time working with pandas' dates. I
+        was forced to use some awkward syntax to get the dates to properly compare. I'm sure there is a better way to
+        do this, but I couldn't figure it out.
+
+        :param mem_id: the member id as a string or int
+        :param start_date: start date as a string in the format mm-dd-yyyy
+        :param end_date: end date as a string in the format mm-dd-yyyy
+        :return: Returns a dataframe containing the information needed to generate a member report or None if the
+        member is not found
+        """
         if isinstance(mem_id, str):
             if len(mem_id) != 9:
                 return None
@@ -259,11 +285,14 @@ class FileSystem:
 
     def add_member(self, member: Member) -> None:
         """
-        Adds a member to the member dataframe
+        Adds a member to the member dataframe. Will automatically assign an id if one is not provided.
+        This does not check for duplicate members, so it is up to the user to ensure that the member is not already
+        in the system otherwise the id will be incremented and the member will be added again.
 
 
-        :param member:
-        :return:
+        :param member: the member to add as a Member object
+        :raises TypeError: if member is not of type Member
+        :return: None on success
         """
         if self._member_df is None:
             self._load_member_df()
@@ -278,6 +307,15 @@ class FileSystem:
                                                            for n in member.__dict__.items() if n[0] != "name"]
 
     def update_member(self, member: Member) -> None:
+        """
+        If the user has a member object that they received from get_member_by_id or get_member_by_name, they can
+        change the member object and then call this function to update the member in the dataframe.
+
+        :param member: a member object that has been updated
+        :raise TypeError: if member is not of type Member
+        :raise ValueError: if the member is not found (should not call this function if the member is not found)
+        :return: None on success
+        """
         if not isinstance(member, Member):
             raise TypeError("member must be of type Member")
 
@@ -293,6 +331,14 @@ class FileSystem:
                                              for n in member.__dict__.items() if n[0] != "name"]
 
     def remove_member(self, member: Member) -> None:
+        """
+        Removes a member from the member dataframe. It is up to the user to ensure that the member is in the system
+        before calling this function.
+
+        :param member: the member to remove as a Member object
+        :raise TypeError: if member is not of type Member
+        :return:
+        """
         if self._member_df is None:
             self._load_member_df()
 
@@ -304,6 +350,14 @@ class FileSystem:
         self._member_df.drop(tmp.index, inplace=True)
 
     def get_provider_by_name(self, last_name: str) -> Provider | None:
+        """
+        Returns a Provider object if found, otherwise returns None. Does not check for duplicate providers, so it is
+        up to the user to ensure that the provider they are getting is the correct one. So be sure to check whether
+
+        the provider has a unique last name.
+        :param last_name: the provider's last name
+        :return: None on failure, Provider on success
+        """
         if self._provider_df is None:
             self._load_provider_df()
 
@@ -315,6 +369,15 @@ class FileSystem:
         return Provider(**provider.iloc[0].to_dict())
 
     def update_provider(self, provider: Provider) -> None:
+        """
+        If the user has a provider object that they received from get_provider_by_id or get_provider_by_name, they can
+        change the provider object and then call this function to update the provider in the dataframe.
+
+        :param provider: a provider object that has been updated
+        :raise TypeError: if provider is not of type Provider
+        :raise ValueError: if the provider is not found (should not call this function if the provider is not found)
+        :return: None on success
+        """
         if not isinstance(provider, Provider):
             raise TypeError("provider must be of type Provider")
 
@@ -330,6 +393,13 @@ class FileSystem:
                                                for n in provider.__dict__.items() if n[0] != "name"]
 
     def get_provider_by_id(self, mem_id: int | str) -> Provider | None:
+        """
+        Returns a Provider object if found, otherwise returns None. This method is preferred over get_provider_by_name
+        because it is guaranteed to return the correct provider.
+
+        :param mem_id: the provider id as a string or int
+        :return: None on failure, Provider on success
+        """
         if isinstance(mem_id, str):
             if len(mem_id) != 9:
                 return None
@@ -347,6 +417,15 @@ class FileSystem:
         return Provider(**provider.iloc[0].to_dict())
 
     def add_provider(self, provider: Provider) -> None:
+        """
+        Adds a provider to the provider dataframe. Will automatically assign an id if one is not provided.
+        This does not check for duplicate providers, so it is up to the user to ensure that the provider is not already
+        in the system otherwise the id will be incremented and the provider will be added again.
+
+        :param provider: the provider to add as a Provider object
+        :raise TypeError: if provider is not of type Provider
+        :return: None on success
+        """
         if self._provider_df is None:
             self._load_provider_df()
 
@@ -360,6 +439,15 @@ class FileSystem:
             n[1] for n in provider.__dict__.items() if n[0] != "name"]
 
     def remove_provider(self, provider: Provider) -> None:
+        """
+        Removes a provider from the provider dataframe. It is up to the user to ensure that the provider is in the
+        system before calling this function.
+
+        :param provider: the provider to remove as a Provider object
+        :raise TypeError: if provider is not of type Provider
+        :raise ValueError: if the provider is not found (should not call this function if the provider is not found)
+        :return: None on success
+        """
         if self._provider_df is None:
             self._load_provider_df()
 
@@ -374,6 +462,12 @@ class FileSystem:
         self._provider_df.drop(tmp.index, inplace=True)
 
     def is_valid_employee(self, emp_id: int | str) -> bool:
+        """
+        Checks if the employee is in the system. This is used to validate the employee id when logging in.
+
+        :param emp_id: the employee id as a string or int
+        :return: True if the employee is in the system, False otherwise
+        """
         if self._employee_df is None:
             self._load_employee_df()
 
@@ -391,6 +485,12 @@ class FileSystem:
         return True
 
     def is_manager(self, emp_id: int | str) -> bool:
+        """
+        Checks if the employee is a manager. This is used to determine if the user has access to the manager menu.
+
+        :param emp_id: the employee id as a string or int
+        :return: True if the employee is a manager, False otherwise
+        """
         if self._employee_df is None:
             self._load_employee_df()
 
@@ -408,6 +508,12 @@ class FileSystem:
         return employee.iloc[0]["is_manager"]
 
     def get_service_name_by_code(self, code: int | str) -> str | None:
+        """
+        Returns the service name associated with the service code. If the service code is not found, None is returned.
+
+        :param code: the service code as a string or int
+        :return: the service name as a string or None if the service code is not found
+        """
         if self._service_directory_df is None:
             self._load_service_df()
 
@@ -425,6 +531,12 @@ class FileSystem:
         return service.iloc[0]["service_name"]
 
     def get_fee_by_code(self, code: int | str) -> float | None:
+        """
+        Returns the fee associated with the service code. If the service code is not found, None is returned.
+
+        :param code: the service code as a string or int
+        :return: a float representing the fee or None if the service code is not found
+        """
         if self._service_directory_df is None:
             self._load_service_df()
 
@@ -442,6 +554,16 @@ class FileSystem:
         return service.iloc[0]["fee"]
 
     def document_service(self, service: Service) -> None:
+        """
+        Adds a service to the all services dataframe. This is used to generate the various reports of the chocAn
+        system. This does not check for duplicate services, so it is up to the user to ensure that the service is not
+        already in the system otherwise the service will be added again. These services are not sorted, so the user
+        should not rely on the order of the services in the dataframe.
+
+        :param service: Service object to add
+        :raise TypeError: if service is not of type Service
+        :return: None on success
+        """
         if not isinstance(service, Service):
             raise TypeError("service must be of type Service")
 
@@ -452,6 +574,17 @@ class FileSystem:
             self._all_services_df.index)] = list(service)
 
     def get_member_report_as_string(self, mem_id: int | str, save_to_file: bool = True) -> str | None:
+        """
+        Returns a string containing an individual member's report formatted as required by the project description.
+        It currently defaults to saving the report to a file, but this can be changed by passing in save_to_file=False.
+        It also filters the report to only include the last 7 days, but this could be changed modifying the function
+        in the future.
+
+
+        :param mem_id: the member id as a string or int
+        :param save_to_file: whether or not to save the report to a file
+        :return: a string containing the member report or None if the member is not found
+        """
         member = self.get_member_by_id(mem_id)
         if member is None:
             return None
@@ -472,6 +605,16 @@ class FileSystem:
         return mem_str
 
     def get_provider_report_as_string(self, prov_id: int | str, save_to_file: bool = True) -> str | None:
+        """
+        Returns a string containing an individual provider's report formatted as required by the project description.
+        It currently defaults to saving the report to a file, but this can be changed by passing in save_to_file=False.
+        It also filters the report to only include the last 7 days, but this could be changed modifying the function
+        in the future.
+
+        :param prov_id: the provider id as a string or int
+        :param save_to_file: whether or not to save the report to a file
+        :return: string containing the provider report or None if the provider is not found
+        """
         provider = self.get_provider_by_id(prov_id)
 
         if provider is None:
@@ -503,6 +646,16 @@ class FileSystem:
         return prov_str
 
     def get_etf_report_as_string(self, prov_id: int | str, save_to_file: bool = True) -> str | None:
+        """
+        Returns a string containing an individual provider's etf statement formatted as required by the project
+        description. It currently defaults to saving the report to a file, but this can be changed by passing in
+        save_to_file=False. It also filters the report to only include the last 7 days, but this could be changed
+        modifying the function in the future.
+
+        :param prov_id: the provider id as a string or int
+        :param save_to_file: Whether or not to save the report to a file
+        :return: string containing the etf report or None if the provider is not found
+        """
         provider = self.get_provider_by_id(prov_id)
 
         if provider is None:
@@ -524,6 +677,15 @@ class FileSystem:
         return etf_str
 
     def get_manager_report_as_string(self, save_to_file: bool = True) -> str | None:
+        """
+        Summarizes the weeks activity for all providers. It currently defaults to saving the report to a file, but
+        this can be changed by passing in save_to_file=False. It also filters the report to only include the last 7
+        days, but this could be changed modifying the function in the future.
+
+
+        :param save_to_file: Whether or not to save the report to a file
+        :return: string containing the manager report
+        """
         if self._all_services_df is None:
             self._load_all_services_df()
 
@@ -565,6 +727,12 @@ class FileSystem:
         return man_str
 
     def get_maximum_member_id(self) -> int:
+        """
+        Modeled after the idea of an auto incrementing primary key in a database. This is used to assign ids to members
+        when they are added to the system.
+
+        :return: the maximum member id in the member dataframe (so add 1 to this to get the next id)
+        """
         if self._member_df is None:
             self._load_member_df()
 
@@ -574,6 +742,12 @@ class FileSystem:
         return self._member_df["id"].max()
 
     def get_maximum_provider_id(self) -> int:
+        """
+        Modeled after the idea of an auto incrementing primary key in a database. This is used to assign ids to providers
+        when they are added to the system.
+
+        :return: the maximum provider id in the provider dataframe (so add 1 to this to get the next id)
+        """
         if self._provider_df is None:
             self._load_provider_df()
 
@@ -583,6 +757,11 @@ class FileSystem:
         return self._provider_df["id"].max()
 
     def get_service_directory_as_string(self) -> str | None:
+        """
+        Returns a string containing the service directory formatted as required by the project description.
+
+        :return: string containing the service directory
+        """
         if self._service_directory_df is None:
             self._load_service_df()
 
@@ -594,6 +773,12 @@ class FileSystem:
         return service_str
 
     def save_dirs(self) -> None:
+        """
+        Very important to note that none of the dataframes are saved until this function is called. This is to ensure
+        that frequent writes to the file system are not made. This function should be called before exiting the program.
+
+        :return:  None
+        """
         if self._member_df is not None:
             self._save_member_df()
 
